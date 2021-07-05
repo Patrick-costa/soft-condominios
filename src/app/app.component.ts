91f1d70fa4e8aa4880cde7bfd12ef3c92d15879b
Técnico Informática <51121742019.1@senacrjedu.local>
2021-07-05 20:36:56 -0300

    acerto de menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,47 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { AutentiticacaoService } from './share/utils/services/autentiticacao.service';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Cadastrar Colaborador', url: 'cad-colaborador', src:"../../assets/icon/cad-morador-branco.svg" },
-  ];
+  dados: any = [];
+  funcao: string;
+  sindicoList: any = [];
+  condominio: any = []
+
   constructor(private authService: AutentiticacaoService,
               private router: Router,
+              private http: HttpClient,
               public menuCtrl: MenuController) {}
 
   logout(){
     this.authService.logout();
     this.menuCtrl.close();
+  }
+
+  ngOnInit() {
+    this.getUser();
+  }
+
+  getUser() {
+    this.http.get(`${environment.baseUrl}/usuarios/auth`).subscribe(x => {
+      let dados = JSON.stringify(x);
+      let usuario = dados;
+      this.dados = JSON.parse(usuario);
+      console.log(this.dados);
+      if(this.dados['funcao']){
+        this.condominio = this.dados.condominio[0];
+        this.funcao = this.dados['funcao'];
+        console.log(this.funcao);
+      } else{
+        this.condominio = this.dados['condominio'];
+      }
+    })
   }
 }
